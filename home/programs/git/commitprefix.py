c533fb6ece627ebadb79fa5a69fa5e23f9a1616b
jmarkin <me@jmarkin.ru>
2024-11-11 00:38:48 +0300

    change commit prefix

--- home/programs/git/commitprefix.py
+++ home/programs/git/commitprefix.py
@@ -12,16 +12,18 @@ prefixes = "(feature|feat|fix|hotfix|refactor|docs|temp)(/|-)"
 issue_ = re.compile(rf"{prefixes}(\w+-\d+).*")
 semantic_ = re.compile(rf"{prefixes}(\w+)")
 
-if matched := re.match(issue_, branch):
-    issue = matched.group(3)
-    with open(commit_msg_filepath, "r+") as fh:
+
+def read_write(file, prefix):
+    with open(file, "r+") as fh:
         commit_msg = fh.read()
         fh.seek(0, 0)
-        fh.write(f"{issue}: {commit_msg}")
+        fh.write(f"{prefix}{commit_msg}")
+
+
+if matched := re.match(issue_, branch):
+    issue = matched.group(3)
+    read_write(commit_msg_filepath, f"{issue}: ")
 elif matched := re.match(semantic_, branch):
     issue = matched.group(3)
     semantic_name = matched.group(1)
-    with open(commit_msg_filepath, "r+") as fh:
-        commit_msg = fh.read()
-        fh.seek(0, 0)
-        fh.write(f"{semantic_name}({issue}): {commit_msg}")
+    read_write(commit_msg_filepath, f"{semantic_name}({issue}): ")
