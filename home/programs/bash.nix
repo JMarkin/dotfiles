7fc0320d0fbed10381c286f9d4567f104d2750cb
jmarkin <me@jmarkin.ru>
2024-11-28 11:11:59 +0300

    tmux as default

--- home/programs/bash.nix
+++ home/programs/bash.nix
@@ -3,12 +3,27 @@
 {
   programs.bash = {
     enable = true;
-    initExtra = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    initExtra = /* bash */''
+      # Do not attach when using a local terminal emulator or tty
+      if [[ "$SSH_CLIENT" == "" || "$TMUX" != "" ]]; then
+
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
       fi
+
+      # Switch to bash when there's arguments exist
+      # such as `scp' or `sftp' or `ssh -t'
+      if [ "$1" != "" ]; then
+          exec -l /bin/bash "$@"
+      fi
+
+      ${pkgs.tmux}/bin/tmux attach || ${pkgs.tmux}/bin/tmux new
+      EXITSTATUS=$?
+      echo -e "\033[0m"
+      exit $EXITSTATUS
     '';
   };
 }
--- home/users/alpine_gw.nix
+++ home/users/alpine_gw.nix
@@ -13,36 +13,4 @@
   home.file = {
     ".config/vpn/template.fish".source = ../legacyconfig/vpn/template.fish;
   };
-
-  programs.bash = {
-    enable = true;
-    initExtra = /* bash */''
-      # Define colors
-      YELLOW="\033[0;93m"
-      GREEN="\033[0;32m"
-      CYAN="\033[0;36m"
-      RED="\033[0;31m"
-      NC="\033[0m"
-
-# Do not attach when using a local terminal emulator or tty
-      if [ "$SSH_CLIENT" == "" ]; then
-              tmux new
-              exit $?
-      fi
-
-# Switch to bash when there's arguments exist
-# such as `scp' or `sftp' or `ssh -t'
-      if [ "$1" != "" ]; then
-              exec -l /bin/bash "$@"
-      fi
-
-# Add a pause for displaying motd
-      echo -e "\n$GREEN[ Press any key to continue ]\n$YELLOW"
-      read -n 1 -s -r
-      tmux attach || tmux new
-      EXITSTATUS=$?
-      echo -e $NC
-      exit $EXITSTATUS
-    '';
-  };
 }
