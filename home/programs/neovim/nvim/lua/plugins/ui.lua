cd7d1bf83b578c0ca851de245e8adb8d38986b15
jmarkin <me@jmarkin.ru>
2025-06-02 09:00:13 +0300

    clean

--- home/programs/neovim/nvim/lua/plugins/ui.lua
+++ home/programs/neovim/nvim/lua/plugins/ui.lua
@@ -1,36 +1,4 @@
 return {
-  -- Better `vim.notify()`
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Dismiss all Notifications",
-      },
-    },
-    opts = {
-      timeout = 1000,
-      stages = "static",
-      level = vim.log.levels.INFO,
-      top_down = true,
-    },
-    config = function(args)
-      args.opts.background_colour = vim.g.notify_background_color
-      require("notify").setup(args.opts)
-      vim.notify = require("notify")
-    end,
-    init = function()
-      vim.notify = function(...)
-        require("lazy").load({ plugins = { "nvim-notify" } })
-        return vim.notify(...)
-      end
-    end,
-  },
-
   -- ui components
   { "MunifTanjim/nui.nvim", lazy = true },
 
@@ -107,23 +75,6 @@ return {
     },
   },
   {
-    "prichrd/netrw.nvim",
-    -- enabled = false,
-    config = function()
-      require("netrw").setup({
-        -- File icons to use when `use_devicons` is false or if
-        -- no icon is found for the given file type.
-        icons = {
-          symlink = "",
-          directory = "",
-          file = "",
-        },
-        -- Uses mini.icon or nvim-web-devicons if true, otherwise use the file icon specified above
-        use_devicons = false,
-      })
-    end,
-  },
-  {
     "lewis6991/whatthejump.nvim",
     -- enabled = false,
     keys = { "<C-i>", "<C-o>" },
@@ -186,16 +137,5 @@ return {
         },
       })
     end,
-  },
-  {
-    "vzze/cmdline.nvim",
-    enabled = false,
-    opts = {
-      cmdtype = ":",
-      column = {
-        maxNumber = 6,
-        minWidth = 20,
-      },
-    },
   },
 }
